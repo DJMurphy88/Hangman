1ccfa92bf7483876234fb032bf63e829ee9a2cd9
Denis Murphy <djmurphy2001@gmail.com>
2021-07-15 10:16:36 -0230

    first commit

---
+++ src/Hangman.java
@@ -0,0 +1,26 @@
+public class Hangman {
+
+    public static void play_game() {
+
+    }
+
+    public static void get_word() {
+
+    }
+
+    public static void draw_screen() {
+
+    }
+
+    public static void add_spaces() {
+
+    }
+
+    public static void get_letter() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Words.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class Words {
+
+    public static void get_random_word() {
+
+    }
+}
